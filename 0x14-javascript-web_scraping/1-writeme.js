35753688b13349581ef67218217649223ddc7035
major0001 <alexmutune746@gmail.com>
2024-06-22 16:33:19 -0700

    writing a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// import the fs module
+const fs = require('fs');
+
+// the function taakes the first and second argument in the command-line process
+const file = process.argv[2];
+const string = process.argv[3];
+
+// checking if file exits
+fs.writeFile(file, string, 'utf-8', (err) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+
+});
