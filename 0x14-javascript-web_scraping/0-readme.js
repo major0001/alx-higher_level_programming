c40417684b8cbbcfb5bcdc278d6624c07dc51ced
major0001 <alexmutune746@gmail.com>
2024-06-22 15:33:54 -0700

    semistandard rules fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -8,17 +8,17 @@ const file = process.argv[2];
 
 // give an error if file does not exist
 if (!file) {
-	console.error('Please provide a file as the first argument.');
-	process.exits(1);
+  console.error('Please provide a file as the first argument.');
+  process.exits(1);
 }
 
 // read the file content in UTF-8 encoding
 fs.readFile(file, 'utf-8', (err, data) => {
-	if (err) {
-		// print the error object ifan error occurred
-		console.error(err);
-		return;
-	}
-	// print the file contents
-	console.log(data);
+  if (err) {
+    // print the error object ifan error occurred
+    console.error(err);
+    return;
+  }
+  // print the file contents
+  console.log(data);
 });
